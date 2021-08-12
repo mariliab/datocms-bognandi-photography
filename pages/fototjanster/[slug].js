01f1e8d8c528fdc83e9c1a7d3a2825c80c988904
mariliab <marilia.bognandi@gmail.com>
2021-08-12 13:12:17 +0200

    request portfolio page info

--- pages/fototjanster/[slug].js
+++ pages/fototjanster/[slug].js
@@ -2,10 +2,57 @@ import { renderMetaTags, useQuerySubscription } from "react-datocms";
 import { request } from "../../lib/datocms";
 import { metaTagsFragment, responsiveImageFragment } from "../../lib/fragments";
 
-export default function PhotoService() {
+export async function getStaticPaths() {
+  const data = await request({ query: `{ allPhotoServicesPages { slug } }` });
+
+  return {
+    paths: data.allPhotoServicesPages.map(
+      (page) => `/fototjanster/${page.slug}`
+    ),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const graphqlRequest = {
+    query: `
+      query PortfolioPageBySlug($slug: String) {
+        photoServicesPage(filter: {slug: {eq: $slug}}) {
+          slug
+          title
+        }
+      }
+    `,
+    preview,
+    variables: {
+      slug: params.slug,
+    },
+  };
+
+  return {
+    props: {
+      subscription: preview
+        ? {
+            ...graphqlRequest,
+            initialData: await request(graphqlRequest),
+            token: process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
+          }
+        : {
+            enabled: false,
+            initialData: await request(graphqlRequest),
+          },
+    },
+  };
+}
+
+export default function PhotoService({ subscription, preview }) {
+  const {
+    data: { photoServicesPage },
+  } = useQuerySubscription(subscription);
+
   return (
     <div>
-      <h1>hej PhotoService</h1>
+      <h1>{photoServicesPage.title}</h1>
     </div>
   );
 }
