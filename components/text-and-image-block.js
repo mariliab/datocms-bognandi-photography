9f0142d43e5118706b64f941ddb7d91ccff53aa6
mariliab <marilia.bognandi@gmail.com>
2021-09-11 21:58:30 +0200

    remove console log

--- components/text-and-image-block.js
+++ components/text-and-image-block.js
@@ -5,8 +5,6 @@ import { createMarkup } from "../utils/help";
 export default function TextAndImageBlock({ data }) {
   const { title, subtitle, text, image, cta } = data;
 
-  console.log("text, ", text);
-
   return (
     <div className="flex flex-col lg:flex-row justify-between items-center">
       <div className="w-full lg:w-2/3 xl:w-1/2 bg-white p-4 md:p-12 2xl:p-24">
