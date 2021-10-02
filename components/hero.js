80bddfa78b579d890bb1b02ddfd6e528de90703c
mariliab <marilia.bognandi@gmail.com>
2021-10-02 09:52:04 +0200

    adjust responsive padding and height on hero block

--- components/hero.js
+++ components/hero.js
@@ -11,16 +11,16 @@ export default function Hero({ title = "", text = "", backgroundImage }) {
           <div className="w-full lg:w-1/2">
             <img
               src={backgroundImage}
-              className="object-cover w-full h-64 max-h-64 lg:h-auto lg:max-h-800px filter grayscale"
+              className="object-cover w-full h-72 max-h-72 lg:h-auto lg:max-h-800px filter grayscale"
             />
           </div>
           <div className="p-4 lg:w-1/2 bg-white md:p-12 2xl:p-24">
             <PageTitle title={title} />
             <div
-              className="mb-8 font-light text-beige-darkest"
+              className="mb-4 lg:mb-8 font-light text-beige-darkest"
               dangerouslySetInnerHTML={createMarkup(text)}
             />
-            <div className="py-4 flex">
+            <div className="py-0 lg:py-4 flex">
               <a
                 href="mailto:marilia@bognandiphotography.com"
                 className="py-4 text-xs tracking-widest font-bold text-center bg-beige-darkest hover:bg-beige-light text-white hover:text-beige-darkest flex-1 rounded-full"
