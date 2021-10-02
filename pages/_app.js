efcea725c73e72074cf3a26524f7c88685805977
mariliab <marilia.bognandi@gmail.com>
2021-10-02 09:17:22 +0200

    add google analytics pageviews

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import { hotjar } from "react-hotjar";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as ga from "../lib/ga";
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   useEffect(() => {
     hotjar.initialize(2629196, 6);
   }, []);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return <Component {...pageProps} />;
 }
