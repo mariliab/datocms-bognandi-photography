26ed158ddb3b7be46b5752cd9513fa2003ae04ab
mariliab <marilia.bognandi@gmail.com>
2021-12-13 22:00:38 +0100

    new layout and styling

--- components/about-me.js
+++ components/about-me.js
@@ -5,8 +5,8 @@ export default function AboutMe({ title = "", text = "", image = null }) {
   return (
     <section className="bg-beige-light py-12 sm:py-24 text-beige-darkest">
       <Container>
-        <div className="flex gap-4 items-stretch max-w-screen-md mx-auto">
-          <div className="w-1/2">
+        <div className="flex-col-reverse flex flex-col lg:flex-row gap-4 items-stretch max-w-screen-md mx-auto">
+          <div className="w-full lg:w-1/2">
             <Image
               data={{
                 ...image,
@@ -15,9 +15,9 @@ export default function AboutMe({ title = "", text = "", image = null }) {
               className="object-cover w-full filter grayscale"
             />
           </div>
-          <div className="bg-white p-4 lg:p-12 flex flex-col justify-center w-1/2">
+          <div className="bg-white p-4 lg:p-12 flex flex-col justify-center w-full lg:w-1/2">
             <div>
-              <div className="text-8xl font-playfair leading-3">"</div>
+              <div className="text-8xl font-montagu font-thin leading-3">"</div>
               <h1 className="mb-2 text-2xl mb-4">{title}</h1>
               <p className="font-light mb-4">{text}</p>
               <p className="font-light italic">/Marilia Bognandi</p>
