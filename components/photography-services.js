8378a5437a17b850c76305fb77db68fe6976b595
mariliab <marilia.bognandi@gmail.com>
2022-05-06 14:38:41 +0200

    new mobile layout for photography services

--- components/photography-services.js
+++ components/photography-services.js
@@ -1,6 +1,7 @@
 import BlockTitle from "../components/block-title";
 import Container from "./container";
 import { Image } from "react-datocms";
+import * as NextImage from "next/image";
 import Link from "next/link";
 
 export default function PhotographyServices({
@@ -23,20 +24,28 @@ export default function PhotographyServices({
               subtitle="Jag erbjuder tre olika typer av fotograferingar. Behöver du detaljbilder på dina produkter, streetstyle porträtt, eller kanske en professionell bild tagen i din arbetsmiljö? Oavsett ditt behov hjälper jag dig att förverkliga din vision!"
               centered
             />
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-1 gap-y-4 md:gap-x-4">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-1 gap-y-4 md:gap-x-4 transform">
               {data.map((item, index) => {
                 return (
                   <Link href={`/fototjanster/${item.slug}`} key={index}>
-                    <div className="flex flex-col justify-center items-center cursor-pointer overflow:hidden">
+                    <div className="flex lg:flex-col justify-center items-center cursor-pointer overflow:hidden gap-4">
                       <Image
                         data={{
                           ...item?.featuredImage?.responsiveImage,
                           alt: item?.featuredImage?.responsiveImage?.title,
                         }}
-                        className="transform transition duration-300 ease-in-out hover:opacity-100 hover:scale-101"
+                        className="w-1/2 lg:w-full transform transition duration-300 ease-in-out hover:opacity-100 hover:scale-101"
                       />
-                      <div className="mt-4 font-normal px-6 pt-3 pb-2 border-1 border-black hover:bg-white transition-all duration-200">
+                      <div className="flex flex-1 justify-center items-center gap-2 font-normal px-6 pt-3 pb-2 border-1 border-black hover:bg-white transition-all duration-200">
                         {item.title}
+                        <div className="flex lg:hidden text-xs leading-none">
+                          <NextImage
+                            src="/images/arrow-thin-right-black.svg"
+                            alt="se mer"
+                            width="12"
+                            height="12"
+                          />
+                        </div>
                       </div>
                     </div>
                   </Link>
