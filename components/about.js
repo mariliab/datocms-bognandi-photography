d4a6d2b3ff7172e5c61a145b306a0fca5a1253d3
mariliab <marilia.bognandi@gmail.com>
2022-05-11 14:57:46 +0200

    update ui on about me page

--- components/about.js
+++ components/about.js
@@ -1,12 +1,9 @@
 import TextAndImageBlock from "../components/text-and-image-block";
-import Container from "./container";
 
 export default function About({ data }) {
   return (
     <section className="bg-beige-lightest py-12 lg:py-24 relative text-beige-darkest">
-      <Container>
-        <TextAndImageBlock data={data?.content[0]} />
-      </Container>
+      <TextAndImageBlock data={data?.content[0]} />
     </section>
   );
 }
--- components/text-and-image-block.js
+++ components/text-and-image-block.js
@@ -7,27 +7,29 @@ export default function TextAndImageBlock({ data }) {
 
   return (
     <div className="flex flex-col lg:flex-row justify-between items-center">
-      <div className="w-full lg:w-2/3 xl:w-1/2 bg-white p-4 md:p-12 2xl:p-24">
-        <BlockTitle title={title} subtitle={subtitle} />
-        <PostBody content={description} />
-        {cta && (
-          <div className="py-4 flex">
-            <a
-              href={cta}
-              className="py-4 text-xs tracking-widest font-bold text-center bg-beige-darkest hover:bg-beige-light text-white hover:text-beige-darkest flex-1 rounded-full"
-            >
-              KONTAKTA MIG
-            </a>
-          </div>
-        )}
-      </div>
-      <div className="w-full lg:w-1/3 xl:w-1/2 relative flex justify-center shadow-lg">
-        <Image
-          data={{
-            ...image?.responsiveImage,
-            alt: image?.responsiveImage.title,
-          }}
-        />
+      <div className="flex-col-reverse flex flex-col lg:flex-row gap-4 items-stretch max-w-screen-xl px-4 md:px-0 mx-auto">
+        <div className="w-full lg:w-1/2">
+          <Image
+            data={{
+              ...image?.responsiveImage,
+              alt: image?.responsiveImage.title,
+            }}
+          />
+        </div>
+        <div className="bg-white p-4 lg:p-12 flex flex-col justify-center w-full lg:w-1/2">
+          <BlockTitle title={title} subtitle={subtitle} />
+          <PostBody content={description} />
+          {cta && (
+            <div className="py-4 flex">
+              <a
+                href={cta}
+                className="py-4 text-xs tracking-widest font-bold text-center bg-beige-darkest hover:bg-beige-light text-white hover:text-beige-darkest flex-1 rounded-full"
+              >
+                KONTAKTA MIG
+              </a>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
