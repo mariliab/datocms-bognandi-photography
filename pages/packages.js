2a4ae0ba7a60b64a945f3bce71252584c903f957
Marilia Bognandi <mariliabognandi@mbpsomtmarilia2.home>
2023-08-30 12:11:35 +0200

    new packages page

---
+++ pages/packages.js
@@ -0,0 +1,170 @@
+import Head from "next/head";
+import { renderMetaTags, useQuerySubscription } from "react-datocms";
+import { request } from "../lib/datocms";
+import { metaTagsFragment, responsiveImageFragment } from "../lib/fragments";
+import Layout from "../components/layout";
+import Container from "../components/container";
+import Navbar from "../components/navbar";
+import PageTitle from "../components/page-title";
+import About from "../components/about";
+
+export async function getStaticProps({ preview }) {
+  const graphqlRequest = {
+    query: `
+      {
+        site: _site {
+          favicon: faviconMetaTags {
+            ...metaTagsFragment
+          }
+        }
+        page(filter: {slug: {eq: "personal-branding-packages"}}) {
+          seo: _seoMetaTags {
+            ...metaTagsFragment
+          }
+          id
+          content {
+            subtitle
+            title
+            image {
+              responsiveImage {
+                ...responsiveImageFragment
+              }
+            }
+            cta
+            description {
+              value
+            }
+          }
+          featuredImage {
+            responsiveImage {
+              ...responsiveImageFragment
+            }
+          }
+          slug
+          subtitle
+          title
+        }
+      }
+      ${metaTagsFragment}
+      ${responsiveImageFragment}
+    `,
+    preview,
+  };
+
+  return {
+    props: {
+      subscription: preview
+        ? {
+            ...graphqlRequest,
+            initialData: await request(graphqlRequest),
+            token: process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
+            environment: process.env.NEXT_DATOCMS_ENVIRONMENT || null,
+          }
+        : {
+            enabled: false,
+            initialData: await request(graphqlRequest),
+          },
+    },
+  };
+}
+
+const packagesData = {
+    pageTitle: "Personal Branding Photography Packages",
+    generalFeatures: [
+      "Flexible styling assistance available upon request to ensure your brand's visual cohesiveness.",
+      "Expert location recommendations provided to match your unique brand identity.",
+      "All packages include natural and authentic high-resolution images that showcase your true essence.",
+    ],
+    packageList: [
+      {
+        title: "Brief Brand Boost Session",
+        description: "Perfect for updating your personal brand visuals.",
+        features: [
+          "Includes 15 natural and authentic high-resolution photos.",
+          "Ideal for profile pictures, feature images, about me pages, and more.",
+        ],
+        investment: "8000 kr",
+      },
+      {
+        title: "Makeover Branding Photoshoot",
+        description: "Level up your branding with a comprehensive photoshoot.",
+        features: [
+          "Content for 2-3 months of consistent 2-3 times per week posting.",
+          "Receive 30 professionally edited high-resolution photos.",
+          "Showcase your brand's evolution with a testimonial and before-and-after example.",
+          "Flexible styling assistance available upon request.",
+          "Expert location recommendations if needed.",
+          "Special Offer: Refer a friend and receive 10% off your next session!",
+        ],
+        investment: "12500 kr",
+      },
+      {
+        title: "ALL IN Package",
+        description: "For those who post daily or several times per week on social media.",
+        features: [
+          "Content for up to 6 months of daily posting.",
+          "Access a versatile 'Image Bank' that captures your brand's essence.",
+          "Say goodbye to impersonal stock images—have your own to maintain authenticity.",
+          "Receive all professionally edited high-res photos from the shoot.",
+          "Personalized consultation to tailor the shoot to your brand's vibe.",
+          "Bonus: Fill out a brand survey to guide the shoot's creative direction.",
+        ],
+        investment: "25000 kr",
+      },
+    ],
+  };
+
+function Packages({ subscription }) {
+  const {
+    data: { site, page },
+  } = useQuerySubscription(subscription);
+
+//   const metaTags = page.seo.concat(site.favicon);
+
+  return (
+    <Layout>
+      <Head>
+      <meta name="robots" content="noindex"></meta>
+      </Head>
+      <Navbar data={site} />
+      <div className="bg-beige-lightest py-12 lg:py-24 relative text-beige-darkest">
+      <Container>
+      <h1 className="text-3xl font-semibold mb-6">{packagesData.pageTitle}</h1>
+      <div>
+        <h2 className="text-xl font-semibold mb-4">General Features:</h2>
+        <ul className="list-none list-inside mb-6">
+          {packagesData.generalFeatures.map((feature, index) => (
+            <li key={index}><span>-</span> {feature}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="grid gap-6 md:grid-cols-3">
+        {packagesData.packageList.map((packageItem, index) => (
+          <div key={index} className="bg-white p-4 lg:p-12 flex flex-col justify-between">
+            <div>
+            <h2 className="uppercase text-xl font-semibold mb-2">{packageItem.title}</h2>
+            <p className="mb-4">{packageItem.description}</p>
+            <ul className="list-none list-inside">
+              {packageItem.features.map((feature, index) => (
+                <li key={index} className="mb-4"><span>-</span> {feature}</li>
+              ))}
+            </ul>
+            </div>
+            <div>
+            <p className="mt-4 font-bold">Investment: {packageItem.investment}</p>
+            <br></br>
+            <a href="mailto:marilia@bognandiphotography.com" className="font-normal px-12 pt-4 pb-3 bg-beige-dark text-white hover:bg-beige-lightest hover:text-beige-darkest transition-all duration-200" target="_blank" rel="noreferrer">I'm ready to book</a>
+            </div>
+          </div>
+        ))}
+      </div>
+      <p className="text-center text-xs mt-4">Prices are ex. VAT</p>
+    </Container>
+    </div>
+    </Layout>
+  );
+}
+
+export default Packages;
+
+
