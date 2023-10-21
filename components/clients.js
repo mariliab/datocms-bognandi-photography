42faf8a09bbf938275a8180d41d4d31e6d587449
Marilia Bognandi <marilia.bognandi@gmail.com>
2023-10-21 13:25:40 +0200

    bug: fix build error

--- components/clients.js
+++ components/clients.js
@@ -1,7 +1,7 @@
 export const Clients = ({ clients = [] }) => {
   return (
     <div className="py-12">
-      <h2 className="mb-4">I've created content for</h2>
+      <h2 className="mb-4">I&apos;ve created content for</h2>
       <div className="flex flex-wrap gap-x-4 gap-y-2 justify-center">
         {clients.map((client, index) => {
           return <p key={index}>{client?.name}</p>;
